db7fcd672d37326e5ff125ec917fdc8b1677a53d
kotiktom <kotikangastommi@hotmail.com>
2019-10-10 16:08:44 +0300

    Fixing problem where page wouldnt refresh carlist after delete

--- src/components/CarList.vue
+++ src/components/CarList.vue
@@ -1,20 +1,15 @@
 <template>
-    <div> 
-<table>
-    <tbody>
-        <tr v-for="(car, index) in cars" v-bind:key="index">
-            <td> {{car.brand}} </td>
-            <td> {{car.model}} </td>
-            <td> {{car.color}} </td>
-            <td> {{car.year}} </td>
-            <td> {{car.price}} </td>
-            <td> {{car.fuel}} </td>
-           <td> <button v-on:click="remove(car)">Delete</button> </td> 
-        </tr>
-    </tbody>
-</table>
-    </div>
+  <v-data-table
+    :headers="headers"
+    :items="cars"
+    class="elevation-1"
+  >
+    <template v-slot:item.action="{item}">
+    <v-btn  v-on:click="remove(item)">Delete</v-btn>
+    </template>
+  </v-data-table>
 </template>
+
 
 <script>
 import axios from 'axios';
@@ -22,17 +17,40 @@ export default {
     
  name:'CarList',
  data() {
+     
      return{
-         cars:[]
-         } 
+    headers: [
+        {
+        text: 'Brand',
+            align: 'left',
+            sortable: false,
+            value: 'brand',
+          },
+          { text: 'Model', value: 'model' },
+          { text: 'Year', value: 'year' },
+          { text: 'Color', value: 'color' },
+          { text: 'Price', value: 'price' },
+          { text: 'Fuel', value: 'fuel' },
+          { text: 'Actions', value:'action', sortable: false},
+          
+          
+      
+    ],   
+         cars:[],
+          } 
          },
          methods: {
-      
-      remove: function(car) {      
-       axios.delete(car._links.car.href)
+        remove: function(car) {
+        axios.delete(car._links.self.href)
+        .then(res => {this.fetch()})        
+      },
 
-       axios.get('https://carstockrest.herokuapp.com/cars')
-        .then(response=> (this.cars= response.data._embedded.cars))} 
+        fetch: function() {
+        axios.get('https://carstockrest.herokuapp.com/cars')
+        .then(response => (this.cars= response.data._embedded.cars))
+        
+      },
+
          },
     
          mounted() {
